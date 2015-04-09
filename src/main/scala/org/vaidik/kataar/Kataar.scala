56c98dbca29e4b766e124c2351e69e176e883eb8
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2015-04-09 21:05:47 +0530

    first commit with some really basic stuff

---
+++ src/main/scala/org/vaidik/kataar/Kataar.scala
@@ -0,0 +1,35 @@
+package org.vaidik.kataar
+
+object Kataar {
+    var conf = Map("path" -> "/var/lib/kataar")
+
+    def configure(conf: Map[String, String]) {
+      // TODO: check how to refer to object variable of the same name
+      this.conf = conf
+    }
+
+    def create (name: String): KataarQueue = {
+      new KataarQueue(name)
+    }
+}
+
+class KataarQueue (name: String) {
+  // The actual queue
+  private var q: List[String] = List()
+
+  def push(item: Any) {
+    // TODO: this is probably error prone. Check better ways of fixing this.
+    this.synchronized {
+      val newQ = List(q, List(item.toString)).flatten
+      q = newQ
+    }
+  }
+
+  def pop(): String = {
+    this.synchronized {
+      val item = q.head
+      q = q.drop(1)
+      item
+    }
+  }
+}
