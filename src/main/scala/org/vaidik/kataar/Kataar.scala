8f49259139e15d952644e7b06686bb7d35416d5d
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2015-04-12 10:18:35 +0530

    update gitignore

--- src/main/scala/org/vaidik/kataar/Kataar.scala
+++ src/main/scala/org/vaidik/kataar/Kataar.scala
@@ -1,6 +1,9 @@
 package org.vaidik.kataar
 
 import com.typesafe.config._
+import java.io.File
+import scala.pickling.Defaults._
+import scala.pickling.json._
 
 object Kataar {
     var config: Config = null
@@ -17,18 +20,35 @@ object Kataar {
 
 class KataarQueue (name: String) {
   // The actual queue
-  private var q: List[String] = List()
+  private var pushQ: List[String] = List()
+  private var popQ: List[String] = List()
   private var size: Int = 0
 
+  private def backup(q: List[String]) {
+    var ts: Long = System.currentTimeMillis / 1000
+    var fileName = this.name
+
+    new Thread(new Runnable {
+      def run() {
+        var pw = new java.io.PrintWriter(new File(Kataar.config.getString("kataar.datadir") + "/" + fileName + ".dump"))
+        pw.write(q.pickle.toString)
+        pw.close()
+      }
+    }).start
+  }
+
   def push(item: Any) {
-    // TODO: this is probably error prone. Check better ways of fixing this.
     this.synchronized {
       if (this.size + 1 > Kataar.config.getInt("kataar.buffer")) {
-        throw new QueueBufferOverflowException("Buffer overflow for queue " + this.name + ".")
+        this.backup(this.pushQ)
+        this.pushQ = List()
+        this.size = 0
+        // throw new QueueBufferOverflowException("Buffer overflow for queue " + this.name + ".")
       }
 
-      val newQ = List(this.q, List(item.toString)).flatten
-      this.q = newQ
+      // TODO: this is probably error prone. Check better ways of fixing this.
+      val newQ = List(this.pushQ, List(item.toString)).flatten
+      this.pushQ = newQ
       size = size + 1
     }
   }
@@ -36,12 +56,12 @@ class KataarQueue (name: String) {
   def pop(): String = {
     this.synchronized {
       try {
-        val item = this.q.head
-        this.q = this.q.drop(1)
+        val item = this.popQ.head
+        this.popQ = this.popQ.drop(1)
         size = size - 1
         item
       } catch {
-        case e: java.util.NoSuchElementException  => {
+        case e: java.util.NoSuchElementException => {
           throw new EmptyQueueException(this.name + " queue is empty.")
         }
       }
